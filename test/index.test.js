83126909241a61b0299768798c0639d5b98b3fb0
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2025-02-13 15:33:10 +0530

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,21 @@
-// test/index.test.js
 const request = require('supertest');
-const app = require('../src/index');
+const app = require('../src/index'); // Import Express app
+const http = require('http');
+
+let server;
+
+beforeAll((done) => {
+  server = http.createServer(app);
+  server.listen(() => done());
+});
+
+afterAll((done) => {
+  server.close(() => done());
+});
 
 describe('GET /', () => {
   it('responds with "Hello, World!"', async () => {
-    const response = await request(app).get('/');
+    const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Hello, World!');
   });
